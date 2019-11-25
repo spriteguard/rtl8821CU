c31eccd08201ad5303a78aba3a09e777d27720b2
Seimon Williams <seimon.williams@wifi-securities.com>
2019-11-25 15:02:11 +0000

    Fixed redefinition of IEEE80211_MAX_AMPDU_BUF macro error.

--- include/wifi.h
+++ include/wifi.h
@@ -967,8 +967,10 @@ typedef enum _HT_CAP_AMPDU_DENSITY {
  * A-PMDU buffer sizes
  * According to IEEE802.11n spec size varies from 8K to 64K (in powers of 2)
  */
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(4, 15, 0)
 #define IEEE80211_MIN_AMPDU_BUF 0x8
 #define IEEE80211_MAX_AMPDU_BUF_HT 0x40
+#endif
 
 
 /* Spatial Multiplexing Power Save Modes */
